0eddc65ed17bb4f4fe0ce7b2969fa32f1473032f
Marcus Hägerstrand <marcus.hagerstrand@gmail.com>
2022-09-07 12:02:38 +0200

    Removed duplicated import of aboutPage in schema.js

--- majornas-bocker/schemas/schema.js
+++ majornas-bocker/schemas/schema.js
@@ -1,30 +1,29 @@
 // First, we must import the schema creator
-import createSchema from "part:@sanity/base/schema-creator";
+import createSchema from 'part:@sanity/base/schema-creator';
 
 // Global imports
-import navigation from "./documents/navigation/navigation";
-import navItem from "./documents/navigation/objects/navItem";
-import link from "./documents/navigation/objects/link";
+import navigation from './documents/navigation/navigation';
+import navItem from './documents/navigation/objects/navItem';
+import link from './documents/navigation/objects/link';
 
 // Home page imports
-import home from "./documents/home/home";
-import hero from "./documents/home/objects/hero";
-import calendar from "./documents/home/objects/calendar";
-import about from "./documents/home/objects/about";
-import news from "./documents/home/objects/news";
+import home from './documents/home/home';
+import hero from './documents/home/objects/hero';
+import calendar from './documents/home/objects/calendar';
+import about from './documents/home/objects/about';
+import news from './documents/home/objects/news';
 
 // About page imports
-import aboutPage from "./documents/about/aboutPage";
-import aboutHero from "./documents/about/objects/aboutHero";
+import aboutPage from './documents/about/aboutPage';
+import aboutHero from './documents/about/objects/aboutHero';
 
 // Then import schema types from any plugins that might expose them
-import schemaTypes from "all:part:@sanity/base/schema-type";
-import aboutPage from "./documents/about/aboutPage";
+import schemaTypes from 'all:part:@sanity/base/schema-type';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
-  name: "default",
+  name: 'default',
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
