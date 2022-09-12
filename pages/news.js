9bd34e9908e5abe188bafd44df2a3732762a8eaf
NellySP <nelly.petren@gmail.com>
2022-09-12 16:04:12 +0200

    add loop for news
    not quite finished

--- pages/news.js
+++ pages/news.js
@@ -3,8 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-import { SanityClient, urlFor } from "next-sanity";
-import { sanityClient } from "../lib/sanity";
+import { sanityClient, urlFor } from "../lib/sanity";
 
 // GROQ query cheat sheet https://www.sanity.io/docs/query-cheat-sheet
 
@@ -13,17 +12,13 @@ const newsPageQuery = `*[_type == 'newsPage']{
 }`;
 
 export default function Home({ newsPage }) {
- const newsHeading = newsPage[0].pageBuilder[0].heading;
- const newsText = newsPage[0].pageBuilder[0].heroDescription;
- const noticeHeading = newsPage[0].pageBuilder[0].noticeOfInterest;
- const noticeText = newsPage [0].pageBuilder[0].noticeText;
+  const newsHeading = newsPage[0].pageBuilder[0].heading;
+  const newsText = newsPage[0].pageBuilder[0].heroDescription;
+  const noticeHeading = newsPage[0].pageBuilder[0].noticeOfInterest;
+  const noticeText = newsPage[0].pageBuilder[0].noticeText;
 
- const singleNews = newsPage [0].pageBuilder[1];
- console.log(singleNews);
-
- { singleNews.map(image => (
-    {image}
- ))} 
+  const singleNews = newsPage[0].pageBuilder;
+  console.log(singleNews);
 
   return (
     <div className={styles.container}>
@@ -37,7 +32,16 @@ export default function Home({ newsPage }) {
         <p>{newsText}</p>
         <h2>{noticeHeading}</h2>
         <p>{noticeText}</p>
-
+        <h4>Här är loopen/galleriet</h4>
+        {/* På nåt sätt exkludera det första resultatet - händelser i butik - mha javascript. Kan man göra det i loopen på något sätt? Also, få tag i bildfan */}
+        {singleNews &&
+          singleNews.map((news) => (
+            <div>
+              {/* <img src={urlFor(news.image.asset._ref).url()} /> */}
+              <p>{news.heading}</p>
+              <p>{news.text}</p>
+            </div>
+          ))}
       </main>
 
       <footer className={styles.footer}></footer>
