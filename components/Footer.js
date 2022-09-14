b8b4d618f8499c1bfded852d1c300679a004fda0
patrosk <naiade99@gmail.com>
2022-09-14 13:04:16 +0200

    added footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
 
-export default function () {
+import { sanityClient } from '../lib/sanity';
+
+export default function Footer() {
+  const [footer, setFooter] = useState([]);
+
+  useEffect(() => {
+      sanityClient
+      .fetch(
+        `*[_type == 'footer'][0]{
+          pageBuilder[]{
+            heading,
+            paragraphs,
+            portableText
+          }
+        }`
+      )
+      .then((data) => setFooter(data))
+    .catch(console.error);
+  }, []);
+  
+  const footerItems = footer.pageBuilder;
+  console.log(footerItems);
+
+  const footerItem1 = footerItems[0];
+  console.log(footerItem1);
+
+
   return (
-    <div id='footer'>
-      <p>Hi yes this is indeed a footer</p>
-    </div>
+    <footer id='footer'>
+      <p>Hi yes this is indeed an awesome footer</p>
+      {/* <p>{heading}</p> */}
+    </footer>
   );
 }
+
+
+// export async function getStaticProps() {
+//   const footer = await sanityClient.fetch(footerQuery);
+
+//   return { props: { footer } };
+// }
