13908cba2ea96d33f487b111c529e03b241333e9
NellySP <nelly.petren@gmail.com>
2022-09-21 12:21:16 +0200

    super minor style changes
    but mostly prettier doing it's thing ...

--- components/CalendarPage/Grid/styles.js
+++ components/CalendarPage/Grid/styles.js
@@ -1,16 +1,27 @@
-import styled from 'styled-components';
-import { device } from '../../../styles/mediaQueries';
+import styled from "styled-components";
+import { device } from "../../../styles/mediaQueries";
 
 export const CalendarWrapper = styled.div`
-display: grid;
-grid-template-columns: repeat(2, 1fr);
-grid-column-gap: 2vw;
-grid-row-gap: 2vw;
-margin-top: 3.3rem;
+  /* mobile first so column? else put below in media etc */
+  display: flex;
+  flex-direction: column;
+
+  /* switch to grid in tablet */
+  @media ${device.tablet} {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-column-gap: 2vw;
+    grid-row-gap: 2vw;
+    margin-top: 3.3rem;
+  }
+
+  @media ${device.laptop} {
+    /* padding-top: 1.5rem;
+    flex: 1; */
+  }
 `;
 
-export const ImgWrapper = styled.div`
-`;
+export const ImgWrapper = styled.div``;
 
 export const ImgFrame = styled.div`
   width: 570px;
@@ -24,13 +35,15 @@ export const SinglePost = styled.div`
 `;
 
 export const PostImg = styled.img`
-  object-fit: fill;
+  /* object-fit: fill; */
+  width: 100%;
 `;
 
 export const PostHeading = styled.div`
   display: flex;
   justify-content: space-between;
   font-family: var(--azeret-mono);
+  margin: 1%;
 
   h3 {
     font-size: var(--h3-desktop);
@@ -39,34 +52,34 @@ export const PostHeading = styled.div`
 `;
 
 export const PostDate = styled.div`
-width: 124px;
-height: 130px;
-background-color: rgba(0, 0, 0, 0.2);
-z-index: 1;
-position: absolute;
-transform: translateY(-130px);
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
-p {
-  color: white;
-  margin: 0;
-  font-family: var(--azeret-mono);
-  font-weight: 200;
-}
-p:nth-child(2) {
-  font-size: 2rem;
-}
+  width: 124px;
+  height: 130px;
+  background-color: rgba(0, 0, 0, 0.2);
+  z-index: 1;
+  position: absolute;
+  transform: translateY(-130px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  p {
+    color: white;
+    margin: 0;
+    font-family: var(--azeret-mono);
+    font-weight: 200;
+  }
+  p:nth-child(2) {
+    font-size: 2rem;
+  }
 `;
 
 export const PostDescription = styled.p``;
 
 export const PostLink = styled.p`
-a {
-  color: black;
-  font-size: var(--small-link-desktop);
-  text-decoration: underline;
-  font-weight: 200;
-}
+  a {
+    color: black;
+    font-size: var(--small-link-desktop);
+    text-decoration: underline;
+    font-weight: 200;
+  }
 `;
