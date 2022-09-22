ed8716d7d83d53acd54addfb7f57ffb45615177b
NellySP <nelly.petren@gmail.com>
2022-09-22 11:28:33 +0200

    fix grid
    grid now responsive, add media queries

--- components/NewsPage/NewsGrid/styles.js
+++ components/NewsPage/NewsGrid/styles.js
@@ -1,64 +1,48 @@
 import styled from "styled-components";
 import { device } from "../../../styles/mediaQueries";
 
-export const NewsWrapper = styled.div `
-display: grid;
-grid-template-columns: repeat(2, 1fr);
-grid-column-gap: 2vw;
-grid-row-gap: 2vw;
-margin-top: 3.3rem;
+export const NewsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  /* switch to grid in tablet */
+  @media ${device.tablet} {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-column-gap: 2vw;
+    grid-row-gap: 2vw;
+    margin-top: 3.3rem;
+  }
 `;
 
-export const ImgWrapper = styled.div `
-max-width: 100%;
-overflow: hidden;
+export const SinglePost = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
 
-export const SinglePost = styled.div `
-display: flex;
-flex-direction:column;
+export const ImgWrapper = styled.div`
+  overflow: hidden;
+  width: 100%;
 `;
 
 export const PostImg = styled.img`
-object-fit: fill;
-
+  object-fit: fill;
+  width: 100%;
 `;
-
 
 export const PostHeading = styled.h3`
-font-size: var(--h3-desktop);
-font-family: var(--azeret-mono);
-font-weight: 400;
+  font-size: var(--h3-desktop);
+  font-family: var(--azeret-mono);
+  font-weight: 400;
 `;
 
-export const PostDescription = styled.p`
-
-`;
+export const PostDescription = styled.p``;
 
 export const PostLink = styled.p`
-
-a {
+  a {
     color: black;
     font-size: var(--small-link-desktop);
     text-decoration: underline;
     font-weight: 200;
-}
-
-`; 
-
-//const StyledNews = styled.div`
-
-//   @media screen and (max-width: 670px) {
-//     .hero-container {
-//       flex-direction: column;
-//     }
-
-//     .news-grid {
-//       grid-template-columns: repeat(1, 1fr);
-//     }
-
-//     h1 {
-//       width: 100%;
-//     }
-//   }
-//`;
+  }
+`;
