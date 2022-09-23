aa1771aaa7b01d06c7d0df0fbd7a557429d5f56b
patrosk <naiade99@gmail.com>
2022-09-23 11:49:25 +0200

    added alt text fields to all images

--- majornas-bocker/schemas/documents/about/objects/aboutHero.js
+++ majornas-bocker/schemas/documents/about/objects/aboutHero.js
@@ -27,6 +27,28 @@ export default {
       option: {
         hotspot: true,
       },
+      fields: [
+        {
+          name: 'caption',
+          type: 'string',
+          title: 'Alt text till bilden',
+          description: 'Beskriv kortfattat vad bilden innehåller',
+          options: {
+            isHighlighted: true, // <-- make this field easily accessible
+          },
+          validation: (Rule) => [
+            Rule.required()
+              .max(80)
+              .error('Undvik att skriva långa alt-texter. Max 80 tecken.'),
+          ],
+        },
+        {
+          // Editing this field will be hidden behind an "Edit"-button
+          name: 'attribution',
+          type: 'string',
+          title: 'Attribution',
+        },
+      ],
     },
     {
       name: "openingHoursTitle",
